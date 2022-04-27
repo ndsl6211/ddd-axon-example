a4842cfda25a5e01550a00159f2a11bbfc2a8abe
ShuMinYang <shuminyang@ShuMinYangMacBookPro.local>
2022-04-28 03:40:11 +0800

    Revised DddApplication to autowire AxonBoardEventHandler.

--- src/main/java/com/example/mashu/DddApplication.java
+++ src/main/java/com/example/mashu/DddApplication.java
@@ -8,13 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DddApplication {
 
-	@Autowired
 	public static AxonBoardEventHandler axonBoardEventHandler;
 
+	@Autowired
+	public DddApplication(AxonBoardEventHandler axonBoardEventHandler) {
+		this.axonBoardEventHandler = axonBoardEventHandler;
+	}
 
 	public static void main(String[] args) {
 
-		System.out.println(axonBoardEventHandler);
+		System.out.println(axonBoardEventHandler.i);
 
 		SpringApplication.run(DddApplication.class, args);
 	}
